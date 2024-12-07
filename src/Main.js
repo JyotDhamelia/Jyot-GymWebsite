b2d169dcb2cefd184e46ba7f15d03a6e7f9c98fb
Jyot Dhamelia <135057024+JyotDhamelia@users.noreply.github.com>
2024-12-07 20:24:44 +0530

    issue fixed

--- src/Main.js
+++ src/Main.js
@@ -76,7 +76,7 @@ function Site() {
     <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 md:mb-0 mb-10">
-            <img className="object-cover object-center rounded" loading="lazy" alt="hero" src={gymImage} />
+            <img className="object-cover object-center rounded" alt="" src={gymImage} />
           </div>
           <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">No Pain No Gain</h1>
